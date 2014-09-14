d0916849b45d40bb677df1cb898201aad40c797e
Ryan Sukale <ryansukale@gmail.com>
2014-09-14 00:45:34 -0700

    Added an http POST hander type for /api/users

--- routes/users.js
+++ routes/users.js
@@ -16,12 +16,20 @@ module.exports = function(app, settings){
 		},
 	];
 
-	usersRouter.get('/:id', function(req, res, next){
-		res.json(users[req.params.id - 1]);
-	});
-
+	// Get a list of all the users
 	usersRouter.get('/', function(req, res, next){
 		res.json(users);
+	});
+
+	// Get the user at an index
+	usersRouter.get('/:index', function(req, res, next){
+		res.json(users[req.params.index]);
+	});
+
+	// Get the user at an index
+	usersRouter.post('/', function(req, res, next){
+		users.push(req.body);
+		res.json(users[users.length - 1]);
 	});
 
 	app.use('/api/users', usersRouter);
